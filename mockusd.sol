d369304de84715d25f22429f76a159b2ff8b3a0b
SahilM2211 <122855735+SahilM2211@users.noreply.github.com>
2025-09-07 11:43:17 +0530

    Add files via upload

---
+++ mockusd.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.0/contracts/access/Ownable.sol";
+
+contract MockUSDC is ERC20, Ownable {
+    constructor() ERC20("Mock USDC", "mUSDC") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
